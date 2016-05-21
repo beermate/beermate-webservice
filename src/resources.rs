1d6ec012055c6262ddcd8170d26ef0c40c076664
Thomas Schedler <thomas@chirimoya.at>
2016-05-21 12:53:38 +0200

    added content type header

--- src/resources.rs
+++ src/resources.rs
@@ -1,6 +1,9 @@
 use iron::prelude::*;
 use iron::status;
+use iron::modifiers;
+use iron::headers;
 use router::Router;
+use rustc_serialize::json;
 
 pub fn get_routes() -> Router {
     let mut router = Router::new();
@@ -9,11 +12,28 @@ pub fn get_routes() -> Router {
     return router;
 }
 
+// Automatically generate `RustcDecodable` and `RustcEncodable` trait
+// implementations
+#[derive(RustcDecodable, RustcEncodable)]
+struct Res {
+    code: u8,
+    status: String,
+}
+
 fn handler(req: &mut Request) -> IronResult<Response> {
     let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
     Ok(Response::with((status::Ok, *query)))
 }
 
 fn index(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "")))
+    Ok(
+        Response::with(
+            (status::Ok,
+            modifiers::Header(headers::ContentType::json()),
+            json::encode(&Res {
+                code: 200,
+                status: "OK".to_string(),
+            }).unwrap())
+        )
+    )
 }
